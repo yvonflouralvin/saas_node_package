6d8227d2eaa1475afe18dbeb965cdf308d444bd9
Yvon Flour <me@yvonflour.com>
2025-07-26 04:19:00 +0200

    add dist

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.doingAny = void 0;
+const doingAny = () => {
+    console.log("doing any");
+};
+exports.doingAny = doingAny;
