65677bbf030e4ea8e919979eaf34b760b6d80351
Alvin Yvon <me@yvonflour.com>
2025-07-26 02:57:44 +0000

    adding index

---
+++ dist/src/index.js
@@ -0,0 +1,7 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.doingAny = void 0;
+const doingAny = (dest) => {
+    console.log("doing any", dest);
+};
+exports.doingAny = doingAny;
